0570c52ef45a11f18d427dde34e459da19a3395c
Waqar-107 <waqar.hassan866@gmail.com>
2020-10-05 20:03:17 +0600

    implementing params

--- Neural Network/neural_network.py
+++ Neural Network/neural_network.py
@@ -10,8 +10,16 @@ train_y = []
 
 number_of_features = None
 number_of_classes = None
+
 test_file_name = "./dataset/testNN.txt"
 train_file_name = "./dataset/trainNN.txt"
+
+number_of_layer = None
+number_of_nodes_in_each_layer = []
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 
 def read_dataset():
@@ -43,5 +51,22 @@ def read_dataset():
         test_y.append(int(values[number_of_features]))
 
 
+def initialize_parameters():
+    global number_of_layer, number_of_nodes_in_each_layer
+    number_of_layer = 3
+    number_of_nodes_in_each_layer = [3, 4, number_of_features]
+
+    assert (number_of_layer == len(number_of_nodes_in_each_layer))
+
+
+def forward_propagation():
+    None
+
+
+def backward_propagation():
+    None
+
+
 if __name__ == "__main__":
     read_dataset()
+    initialize_parameters()
